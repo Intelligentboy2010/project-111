e2ca7c410baeac93ee73e1126eb9c87e42f58af4
Intelligentboy2010 <86296033+Intelligentboy2010@users.noreply.github.com>
2024-02-07 16:47:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -23,9 +23,47 @@ function modelLoaded(){
     console.log('Model Loaded!');
 }
 
+function check(){
+    img=document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error,results){
+    if (error){
+        console.log(error);
+    
+    }else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML=results[0].label;
+       gesture=results[0].label;
+       toSpeak="";
+        speak();
+        if(gesture=="victory"){
+            document.getElementById("result_object_gesture_icon").innerHTML="&#9996;";
+            toSpeak="That was the Marvelous victory";
+        }
+         else if(gesture=="best"){
+            document.getElementById("result_object_gesture_icon").innerHTML="&#128076;";
+            toSpeak="All the best";
+        }
+        else if(gesture=="amazing"){
+            document.getElementById("result_object_gesture_icon").innerHTML="&#128077;";
+            toSpeak="This is looking amazing";
+       
+    }
+    speak();
+
+}
+
+}
 
 
-
+function speak(){
+    var synth=window.speechSynthesis;
+    speak_data=toSpeak;
+    var utterThis=new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
 
 
 
